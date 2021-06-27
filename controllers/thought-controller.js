79b867a69ca3835b262d514315aab76bb24a5d76
ItsEvdok <semionaevdokimov@icloud.com>
2021-06-27 03:37:49 -0400

    added comments to map out the game plan for routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -3,3 +3,13 @@ const { User, Thought } = require("../models");
 const thoughtController = {};
 
 module.exports = thoughtController;
+
+// GET all thoughts
+// GET a single thought by id
+// POST to create a new thought
+// PUT to update a thought by ID
+// DELETE to remove thought by ID
+
+// /api/thoughts/:thoughtId/reactions
+// POST to create a reaction
+// DELETE to pull and remove a reaction
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,3 +3,13 @@ const { User, Thought } = require("../models");
 const userController = {};
 
 module.exports = userController;
+
+// GET All users
+// GET a single user
+// POST a new user
+// PUT to update a user by id
+// DELETE a user by id
+
+// /api/users/:userId/friends/:friendId
+// POST to add a new friend
+// DELETE to remove a friend
