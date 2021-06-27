efadb3125008d29e9a7f6ac6cc23d1a56308d74c
ItsEvdok <semionaevdokimov@icloud.com>
2021-06-27 04:15:05 -0400

    user controller set up

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,15 +1,70 @@
 const { User, Thought } = require("../models");
 
-const userController = {};
+const userController = {
+    // GET all users
+    getAllUsers(req, res) {
+        User.find({}).then((dbUserData) => {
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // GET single user by ID
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: ( "thoughts","friends" ),
+            select: "-__v"
+        })
+        .select("-__v")
+        .then((dbUserData) => {
+            if (!dbUserData) {
+                res.status(404).json({ message: "No User found with this id!" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+    // POST new user
+    createUser({body}, res){
+        User.create(body)
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err) => res.status(400).json(err));
+    },
+
+    // UPDATE user by id
+    updateUser({params, body}, res) {
+        User.findOneAndUpdate({_id: params.id}, body, { new:true })
+        .then((dbUserData) => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No User found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+    // DELETE user
+    deleteUser({params}, res){
+        User.fineOneAndDelete({ _id:params.id })
+        .then((dbUserData) => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No User found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+};
 
 module.exports = userController;
-
-// GET All users
-// GET a single user
-// POST a new user
-// PUT to update a user by id
-// DELETE a user by id
-
-// /api/users/:userId/friends/:friendId
-// POST to add a new friend
-// DELETE to remove a friend
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,4 +2,8 @@ const router = require("express").Router();
 
 const {} = require("../../controllers/user-controller");
 
+// /api/users/:userId/friends/:friendId
+// POST to add a new friend
+// DELETE to remove a friend
+
 module.exports = router;
